6bcffc3175c239fde0234574ccafbe3bc54f8f16
awesomebunny <awesomebunny@bellsouth.net>
2024-04-19 16:40:56 -0400

    Exercise 1.4

--- Part 1/1.1-1.2/src/App.jsx
+++ Part 1/1.1-1.2/src/App.jsx
@@ -48,28 +48,28 @@ const App = () => {
 
   const course = 'Half Stack application development'
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
 
 
   return (
     <>
-      <Header course={course} />
-      <Content part1Name = {part1.name} part1Exercises = {part1.exercises} part2Name = {part2.name} part2Exercises = {part2.exercises} part3Name = {part3.name} part3Exercises = {part3.exercises}/>
-      <Total exercises = {[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Header course = {course} />
+      <Content parts = {parts} />
+      <Total parts = {parts}/>
      </>
   );
 };
