67294344ef0e4be3f94334752b04373c756f1141
awesomebunny <awesomebunny@bellsouth.net>
2024-08-19 13:36:37 -0400

    Exercise 2.8

--- Part 2/2.6-2.10/src/App.jsx
+++ Part 2/2.6-2.10/src/App.jsx
@@ -8,56 +8,90 @@ const App = ({phoneBook}) => {
 	// ]);
 
 
-	const [phoneNumbers, setName] = useState(phoneBook);
+	const [phoneNumbers, setEntry] = useState(phoneBook);
 
 	const [newName, setNewName] = useState('');
+
+	const [newNumber, setNewNumber] = useState('');
 
 	const addNumber = (event) => {
 		event.preventDefault();
 		const newEntry = {
 			name: newName,
+			number: String(newNumber),
 			favorite: Math.random() < 0.5,
 			id: String(phoneNumbers.length + 1)
 		};
 
-		const found = phoneNumbers.find((entry) => entry.name === newName);
+		let nameExisting = findExisting("name", newName, newEntry);
+		let numberExisting = findExisting("number", newNumber, newEntry);
 
-		console.log(found);
 
-		if (found) {
-			alert(`${newName} is already added to the phonebook`);
-		} else {
-			setName(phoneNumbers.concat(newEntry));
-		}
+		if (!nameExisting && !numberExisting) {
+			setEntry(phoneNumbers.concat(newEntry));
 
-		setNewName("");
+			setNewName("");
+			setNewNumber("");
+	
+			document.getElementById("name-input").value = "";
+			document.getElementById("number-input").value = "";
+		};
 
+	
+
+		function findExisting(property, value) {
+
+			const foundName = phoneNumbers.find((entry) => entry[property] === value);
+
+			console.log(foundName);
+	
+			if (foundName) {
+				alert(`${value} is already added to the phonebook`);
+				return true;
+			} else {
+				return false;
+			};
+	
+		};
+		
 	};
 
 	const handleInputUpdate = (event) => {
 		console.log(event.target.value);
-		setNewName(event.target.value);
+
+		if (event.currentTarget.id === "name-input") {
+			setNewName(event.target.value);
+		} else if (event.currentTarget.id === "number-input") {
+			setNewNumber(event.target.value);
+		};
 	}
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
+
 			<form onSubmit={addNumber}>
 				<div>
-					name: <input defaultValue={newName} onChange={handleInputUpdate}/>
+					Name: <input id="name-input" defaultValue={newName} onChange={handleInputUpdate}/>
 				</div>
+				<>
+					Number: <input id="number-input" defaultValue={newNumber} onChange={handleInputUpdate}/>
+				</>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
+
 			<h2>Numbers</h2>
+
 			{phoneNumbers.map(phoneNumber => 
 				<Entry key={phoneNumber.id} entry={phoneNumber}/>
 			)}
+
 			<br/>
 			<br/>
 			<>
-				<i>Debug: {newName}</i>
+				<i>Debug: {newName} - {newNumber}</i>
 			</>
 		</div>
 	);
